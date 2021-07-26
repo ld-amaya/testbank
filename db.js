a3a51c68b50b8e6e8cf7c1222aafc11a9788cae8
lou <louamaya@me.com>
2021-07-26 13:21:27 -0700

    create database conection

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+const { getDatabaseURL } = require('./config');
+
+let db;
+
+if (process.env.NODE_ENV === 'production') {
+    db = new Client({
+        connectionString = getDatabaseURL(),
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    db = new Client({
+        connectionString = getDatabaseURL()
+    });
+}
+
+db.connect();
+
+module.exports = db;

1e28cb40bdf69809394af566472767b03e42723e
lou <louamaya@me.com>
2021-07-27 00:35:58 -0700

    add databse connection

--- db.js
+++ db.js
@@ -5,14 +5,14 @@ let db;
 
 if (process.env.NODE_ENV === 'production') {
     db = new Client({
-        connectionString = getDatabaseURL(),
+        connectionString: getDatabaseURL(),
         ssl: {
             rejectUnauthorized: false
         }
     });
 } else {
     db = new Client({
-        connectionString = getDatabaseURL()
+        connectionString: getDatabaseURL()
     });
 }
 

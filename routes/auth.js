236b071d2693d68582d4636411f5519d622f53f2
lou <louamaya@me.com>
2021-08-02 03:20:31 -0700

    add post request for new user registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,2 +1,25 @@
 "use strict"
 
+const express = require("express");
+const User = require('../models/User');
+const jsonschema = require('jsonschema');
+const newUserSchema = require('../schemas/newUserSchema.json');
+const { BadRequestError } = require("../expressError");
+
+const router = express.Router();
+
+router.post("/register", async (req, res, next) => {
+    try {
+        const validator = jsonschema.validate(req.body, newUserSchema);
+        if (!validator.valid) {
+            const errs = validator.errs.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+        // Add user to database
+        const newUser = await User.register({ ...req.body });
+        return res.json({ user: newUser });
+    } catch (err) {
+        return next(err);
+    }
+})
+module.exports = router;

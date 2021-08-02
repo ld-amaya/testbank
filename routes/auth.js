37df7726bbf15e2a9713040f6589f41ad9e86f06
lou <louamaya@me.com>
2021-08-02 14:38:03 -0700

    add registration and login handle to manaage authentication, registration and token for users

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,32 @@ const express = require("express");
 const User = require('../models/User');
 const jsonschema = require('jsonschema');
 const newUserSchema = require('../schemas/newUserSchema.json');
+const userLoginSchema = require('../schemas/userLoginSchema.json');
 const { BadRequestError } = require("../expressError");
+const { createToken } = require('../helpers/token');
 
 const router = express.Router();
 
-router.post("/register", async (req, res, next) => {
+/** User login */
+router.post('/login', async (req, res, next) => {
+    try {
+        const validator = jsonschema.validate(req.body, userLoginSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+        // Verify user credentials in database
+        const user = await User.authenticate(req.body);
+        //Create token for the user
+        const token = createToken(user);
+        return res.json({ token });
+    } catch (err) {
+        return next(err);
+    }
+})
+
+/** New user registration */
+router.post('/register', async (req, res, next) => {
     try {
         const validator = jsonschema.validate(req.body, newUserSchema);
         if (!validator.valid) {
@@ -17,7 +38,9 @@ router.post("/register", async (req, res, next) => {
         }
         // Add user to database
         const newUser = await User.register({ ...req.body });
-        return res.json({ user: newUser });
+        //Create token for the user
+        const token = createToken(newUser);
+        return res.json({ token });
     } catch (err) {
         return next(err);
     }

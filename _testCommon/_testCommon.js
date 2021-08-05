edfd04c7d0125cb0bcc89a738189c64f4e530785
lou <louamaya@me.com>
2021-08-05 16:05:19 -0700

    add more dummy topics

--- _testCommon/_testCommon.js
+++ _testCommon/_testCommon.js
@@ -10,11 +10,18 @@ async function commonBeforeAll() {
     await db.query("DELETE FROM topics");
     await db.query("DELETE FROM users");
 
-    const topicId = await db.query(`
+    let topicId = await db.query(`
     INSERT INTO topics (topic) VALUES ('testTopic')
     RETURNING id`);
 
     tid.push(+(topicId.rows[0].id))
+
+    topicId = await db.query(`
+    INSERT INTO topics (topic) VALUES ('testTopic2')
+    RETURNING id`);
+
+    tid.push(+(topicId.rows[0].id))
+    
     let res = await db.query(`INSERT INTO questions (topic_id, question, images, a, b, c, d, answer)
                     VALUES (${tid[0]},'question 1?',null,'a','b','c','d','a')
                     RETURNING id`);

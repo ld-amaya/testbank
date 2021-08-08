90c499f149ac4f3d85bb7341dc1f036b9e87dc35
lou <louamaya@me.com>
2021-08-08 16:13:13 -0700

    add more routes for exam, patch, delete

--- routes/exam.js
+++ routes/exam.js
@@ -1,15 +1,58 @@
 const express = require('express');
-const router = express.Router();
+const Exam = require("../models/exam");
+const Summary = require("../models/summary");
 
+const router = express.Router();
 const { UnauthorizedError } = require("../expressError");
 const {
     ensureUserLoggedIn,
-    ensureUserIsTeacher
+    ensureUserIsTeacher,
+    ensureCorrectUserLogged
 } = require('../middleware/auth');
 
+/** 
+ * Students retrieves data  of an exam based on number 
+ * Should return question, choices, image
+ * */
+router.get("/student/:username/exam/:examId/:num", ensureCorrectUserLogged, async (req, res, next) => {
+    const { examId, num } = req.params
+    const question = await Exam.get(examId, num);
+    res.status(200).json({ question });
+})
 
-router.post("/test", async (req, res, next) => {
-  
+/** 
+ * Students takes an exam 
+ * Generate exam exam id
+ * Returns all question selected randomly
+ * */
+router.post("/student/:username/topic/:topic", ensureCorrectUserLogged, async (req, res, next) => {
+    const userID = res.locals.user.id
+    const questions = await Exam.create(req.params.topic,userID);
+    res.status(201).json({questions})
+})
+
+/** 
+ * Student submits exam as done 
+ * Returns exam result / student score
+ * */
+router.patch("/student/:username/exam/:examId/finished", ensureCorrectUserLogged, async (req, res, next) => {
+    const examId = req.params.examId
+    const score = await Exam.done(examId);
+    const result = await Summary.save(examId, score)
+    res.status(201).json({result})
+});
+
+/** 
+ * Student submits the answer 
+ * Return student's answer
+ * */
+router.patch("/student/:username/exam/:examId/:num", ensureCorrectUserLogged, async (req, res, next) => {
+    const { examId, num } = req.params
+    // Check if user is done or not
+    const isDone = await Summary.isDone(examId);
+    if (isDone) res.status(200).json({ result: 'done' });
+    const answer = await Exam.save(examId, num, req.body.answer);
+    res.status(200).json({ answer });
 })
 
 module.exports = router;

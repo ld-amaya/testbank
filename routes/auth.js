88e65c6175c84e9b80bf9da2e02e6b347d3dae98
lou <louamaya@me.com>
2021-08-08 00:08:43 -0700

    refactoring json schema pointing to helpers/checkSchema

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const Auth = require('../models/auth');
 const jsonschema = require('jsonschema');
 const newUserSchema = require('../schemas/newUserSchema.json');
 const userLoginSchema = require('../schemas/userLoginSchema.json');
+const checkSchema = require("../helpers/checkSchema");
 const { BadRequestError } = require("../expressError");
 const { createToken } = require('../helpers/token');
 
@@ -13,11 +14,7 @@ const router = express.Router();
 /** User login */
 router.post('/login', async (req, res, next) => {
     try {
-        const validator = jsonschema.validate(req.body, userLoginSchema);
-        if (!validator.valid) {
-            const errs = validator.errors.map(e => e.stack);
-            throw new BadRequestError(errs);
-        }
+        await checkSchema(req.body, userLoginSchema);
         // Verify user credentials in database
         const user = await Auth.authenticate(req.body);
         // Create token for the user
@@ -31,11 +28,7 @@ router.post('/login', async (req, res, next) => {
 /** New user registration */
 router.post('/register', async (req, res, next) => {
     try {
-        const validator = jsonschema.validate(req.body, newUserSchema);
-        if (!validator.valid) {
-            const errs = validator.errors.map(e => e.stack);
-            throw new BadRequestError(errs);
-        }
+        await checkSchema(req.body, newUserSchema);
         // Add user to database
         const newUser = await Auth.register({ ...req.body });
         // Create token for the user
